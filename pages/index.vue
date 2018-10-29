7834afc4935c353e83b48f0a31067ed134401b44
SeiLan0123 <seiran0123@gmail.com>
2018-10-29 10:15:59 +0900

    initial commit

---
+++ pages/index.vue
@@ -0,0 +1,9 @@
+<template>
+  <h1>This is ranking page</h1>
+
+</template>
+
+<script>
+import { mapMutations } from "vuex";
+export default {};
+</script>
---
+++ pages/login.vue
@@ -0,0 +1,60 @@
+<template>
+  <section class="container">
+    <div>
+      <h1 class="title">
+        mybest
+      </h1>
+      <div class="links">
+        <input type="button" class="button--green" value="Login" @click="doLogin">
+      </div>
+    </div>
+  </section>
+</template>
+
+<script>
+import firebase from "@/plugins/firebase";
+import { mapActions } from "vuex";
+export default {
+  async mounted() {
+    let user = await new Promise((resolve, reject) => {
+      firebase.auth().onAuthStateChanged(user => resolve(user));
+    });
+    this.setUser(user);
+    if (user) {
+      this.$router.push("/");
+    }
+  },
+  methods: {
+    ...mapActions(["login", "setUser"]),
+    doLogin() {
+      this.login()
+        .then(() => console.log("resolved"))
+        .catch(err => console.log(err));
+    }
+  }
+};
+</script>
+
+<style>
+.container {
+  min-height: 100vh;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  text-align: center;
+}
+
+.title {
+  font-family: "Quicksand", "Source Sans Pro", -apple-system, BlinkMacSystemFont,
+    "Segoe UI", Roboto, "Helvetica Neue", Arial, sans-serif; /* 1 */
+  display: block;
+  font-weight: 300;
+  font-size: 200px;
+  color: #35495e;
+  letter-spacing: 1px;
+}
+
+.links {
+  padding-top: 15px;
+}
+</style>
