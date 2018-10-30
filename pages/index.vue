a6818c600991d1d9ddf07ea9b2cc10bca1388b52
SeiLan0123 <seiran0123@gmail.com>
2018-10-30 17:42:07 +0900

    2

--- pages/index.vue
+++ pages/index.vue
@@ -1,9 +1,29 @@
 <template>
+  <section>
   <h1>This is ranking page</h1>
-
+  <nuxt-link to="newdata">データを追加する</nuxt-link>
+ {{this.$store.state.user.displayName}}
+  </section>
 </template>
 
 <script>
-import { mapMutations } from "vuex";
-export default {};
+import firebase from "@/plugins/firebase";
+var obj;
+export default {
+  data(context) {
+    return { obj };
+  },
+  created: function() {
+    firebase
+      .database()
+      .ref("mybest/ranking/1")
+      .once("value")
+      .then(result => {
+        if (result.val()) {
+          obj = result.val();
+        }
+        console.log(obj);
+      });
+  }
+};
 </script>
---
+++ pages/newdata.vue
@@ -0,0 +1,38 @@
+<template>
+  <section>
+    <h1>新しいデータを追加しよう</h1>
+    <nuxt-link to="/">ランキングに戻る</nuxt-link>
+    <p>品目<input type="text" v-model="item"></p>
+    <p>点数<input type="number" v-model="num"></p>
+    <p>店名<input type="text" v-model="plc"></p>
+    <button @click="add(item, num, plc)">追加</button>
+    </section>
+</template>
+
+
+<script>
+import firebase from "@/plugins/firebase";
+var item;
+var num;
+var plc;
+
+export default {
+  data() {
+    return { item, num, plc };
+  },
+  methods: {
+    add() {
+      var database = firebase.database();
+      firebase
+        .database()
+        .ref("mybest/ranking/1")
+        .set({
+          item: this.item || " ",
+          scr: this.num || 0,
+          plc: this.plc || " "
+        });
+      console.log("add");
+    }
+  }
+};
+</script>
