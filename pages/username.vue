6cedf64d786678d917ebd164da3971cd962aa60b
SeiLan0123 <seiran0123@gmail.com>
2018-11-30 13:30:35 +0900

    check username

--- pages/username.vue
+++ pages/username.vue
@@ -1,5 +1,13 @@
 <template>
   <section>
+    <v-alert
+    color="red"
+    v-model="alert"
+    dismissible
+    type="error"
+    >
+    This username already used
+    </v-alert>
     <h1>
       ユーザーネームを決めてください
     </h1>
@@ -15,19 +23,39 @@
 <script>
 import firebase from "firebase";
 import "vuetify/dist/vuetify.min.css";
+
+var database = firebase.database();
+
 export default {
   data() {
-    return { userName: null };
+    return {
+      userName: null,
+      alert: false
+    };
   },
 
   methods: {
     register() {
-      console.log("register");
-      firebase
-        .database()
-        .ref("mybest/user/" + this.$store.state.user.uid)
-        .set({
-          username: this.userName
+      database
+        .ref("mybest/username/" + this.userName)
+        .once("value")
+        .then(result => {
+          if (!result.val()) {
+            console.log(result.val());
+
+            database.ref("mybest/user/" + this.$store.state.user.uid).set({
+              username: this.userName
+            });
+            console.log("success!!");
+
+            database.ref("mybest/username/" + this.userName).set(true);
+          } else {
+            console.log("failed");
+            this.alert = true;
+          }
+        })
+        .catch(err => {
+          console.log(err);
         });
     }
   },
@@ -36,6 +64,7 @@ export default {
       this.$router.push("login");
     }
   },
+  computed: {},
 
   layout: "empty"
 };
